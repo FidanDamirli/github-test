9a4ca3555f2edc6817cb57d267cb8c5bd66a7806
Fidan <fidan@MacBook-Pro-Fidan.local>
2024-05-14 16:13:34 +0400

    new app for codeacademy

--- Program.cs
+++ Program.cs
@@ -3,7 +3,7 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, My new branch!!!");
+        Console.WriteLine("Hello, My new codeacademy!!!");
     }
 }
 
